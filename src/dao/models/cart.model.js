5867b0558daf7234847caeae908040022e386641
Diego Fernandez <diego.fnicolini@gmail.com>
2024-03-28 22:24:58 +0100

    Segunda Entrega

---
+++ src/dao/models/cart.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema({
+    quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+    stock: Number,
+    total: Number,
+    country: String,
+    state: String,
+    city: String,
+    street: String,
+    postal_code: Number,
+    phone: Number,
+    card_Bank: Number,
+    security_Number: Number,
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    },
+});
+
+const Cart = mongoose.model("Cart", cartSchema);
+
+export default Cart;
